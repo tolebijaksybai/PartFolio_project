4fadc928951226aa6651eb3ee99795ed122582fb
Tolebi Zhaksybay <52714747+tolebijaksybai@users.noreply.github.com>
2020-08-21 18:20:15 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project-pagination',
+        bulletClass: 'project-bullet',
+        bulletActiveClass: 'project-bullet-active',
+        clickable: true
+      },
+});
